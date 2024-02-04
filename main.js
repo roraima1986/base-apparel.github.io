78e1d758636211b04573e2c65ec0a8d82d435720
roraima1986 <96786551+roraima1986@users.noreply.github.com>
2024-02-04 15:17:51 -0300

    Javascript y Readme

--- main.js
+++ main.js
@@ -1,3 +1,27 @@
 document.addEventListener('DOMContentLoaded', () => {
     
+    const emailInput = document.querySelector('#email')
+    const btn = document.querySelector('#form-btn')
+    const iconError = document.querySelector(".form__icon-error")
+    const msgError = document.querySelector(".form__msg")
+
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/
+
+    btn.addEventListener('click', e => {
+        e.preventDefault()
+
+        if(emailRegex.test(emailInput.value)) {
+            iconError.style.display = 'none'
+            msgError.style.display = 'none'
+            emailInput.value = ''
+
+            setTimeout(() => {
+                alert('Email sent')
+            }, 1000)
+        } else {
+            iconError.style.display = 'block'
+            msgError.style.display = 'block'
+        }
+    })
+    
 })
